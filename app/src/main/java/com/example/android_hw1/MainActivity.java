f74040bf669f2a8824d33f47266656897cb4435f
NikKonshin <konbshin1996@gmail.c0m>
2020-08-12 23:00:25 +0300

    hw1

--- app/src/main/java/com/example/android_hw1/MainActivity.java
+++ app/src/main/java/com/example/android_hw1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android_hw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView cityText = findViewById(R.id.city_text);
+        final TextView weatherText = findViewById(R.id.weather_text);
+        Button buttonMoscow = findViewById(R.id.button_Moscow);
+        Button buttonPerm = findViewById(R.id.button_Perm);
+        Button buttonPetersburg = findViewById(R.id.button_StPetersburg);
+        Button buttonOmsk = findViewById(R.id.button_Omsk);
+
+
+        buttonMoscow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                cityText.setText(R.string.city_moscow);
+                weatherText.setText(R.string.weather_moscow);
+            }
+        });
+
+        buttonPerm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                cityText.setText(R.string.button_perm);
+                weatherText.setText(R.string.weather_perm);
+            }
+        });
+
+        buttonPetersburg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                cityText.setText(R.string.button_petersburg);
+                weatherText.setText(R.string.weather_petersburg);
+            }
+        });
+
+        buttonOmsk.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                cityText.setText(R.string.button_omsk);
+                weatherText.setText(R.string.weather_omsk);
+            }
+        });
+
+
+
+
     }
+
+
 }
